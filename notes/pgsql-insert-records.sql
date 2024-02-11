a790c0c1bccf3d37ba3b2a4e6af805d37cadfb43
baimiyishu13 <baimiyishu13@163.com>
2024-02-11 20:10:04 +0800

    pgsql-insert-records

---
+++ notes/pgsql-insert-records.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (age, firstname, lastname, email) 
+VALUES (30, 'jon', 'jontom', 'jontom@test.com');
+
+#lenslocked=#  SELECT * FROM users;
+# id | age | firstname | lastname |      email      
+#----+-----+-----------+----------+-----------------
+#  1 |  30 | jon       | jontom   | jontom@test.com
