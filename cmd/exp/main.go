e1a333e09594d93a73756e1836d7d702874f4f8a
baimiyishu13 <baimiyishu13@163.com>
2024-02-17 12:34:55 +0800

    query-single-record

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -45,24 +45,88 @@ func main() {
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Exec failed: %v\n", err)
-		// os.Exit(1)
-		return
+		os.Exit(1)
 	}
 
 	// Insert some date ...
-	name := "','');DROP TABLE users CASCADE; --"
-	email := "JonD@gmail.com"
-	// queru := fmt.Sprintf(`
-	// 	Insert into users (name, email)
-	// 	VALUES ('%s', '%s');
-	// `, name, email)
-	// fmt.Printf("Execqueru: %s\n", queru)
-	// _, err = dbpool.Exec(context.Background(), queru)
-	_, err = dbpool.Exec(context.Background(), `
-	INSERT INTO users (name, email)
-	VALUES ($1, $2);`, name, email)
+	// // name := "','');DROP TABLE users CASCADE; --"
+	name := "New User2"
+	email := "NewUser2@gmail.com"
+	// // queru := fmt.Sprintf(`
+	// // 	Insert into users (name, email)
+	// // 	VALUES ('%s', '%s');
+	// // `, name, email)
+	// // fmt.Printf("Execqueru: %s\n", queru)
+	// // _, err = dbpool.Exec(context.Background(), queru)
+	// row := dbpool.QueryRow(context.Background(), `
+	// INSERT INTO users (name, email)
+	// VALUES ($1, $2) RETURNING id, name;`, name, email)
+	// var id int
+	// err = row.Scan(&id, &name)
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "Exec failed: %v\n", err)
+	// 	os.Exit(1)
+	// }
+	// fmt.Println("User inserted with id: ", id, "User name: ", name)
+
+	id := 1
+	row := dbpool.QueryRow(context.Background(), `
+		SELECT name,email 
+		FROM users 
+		WHERE id=$1`, id)
+	err = row.Scan(&name, &email)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Exec failed: %v\n", err)
 		os.Exit(1)
 	}
+	fmt.Println("User name: ", name, "User email: ", email)
+
+	// userID := 1
+	// for i := 1; i <= 5; i++ {
+	// 	amonut := i * 100
+	// 	desc := fmt.Sprintf("Order %d", i)
+	// 	_, err := dbpool.Exec(context.Background(), `
+	// 	INSERT INTO orders (
+	// 		user_id, amonut, description)
+	// 		VALUES ($1, $2, $3);`, userID, amonut, desc)
+	// 	if err != nil {
+	// 		fmt.Fprintf(os.Stderr, "Exec failed: %v\n", err)
+	// 		os.Exit(1)
+	// 	}
+	// }
+	// fmt.Println("Created 5 orders for user 1")
+
+	type order struct {
+		ID          int
+		UserID      int
+		Amonut      int
+		Description string
+	}
+	var orders []order
+	userID := 1
+	rows, err := dbpool.Query(context.Background(), `
+        SELECT id, user_id, amonut, description
+        FROM orders 
+        WHERE user_id=$1`, userID)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Exec failed: %v\n", err)
+		os.Exit(1)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var o order
+		err = rows.Scan(&o.ID, &o.UserID, &o.Amonut, &o.Description)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Exec failed: %v\n", err)
+			os.Exit(1)
+		}
+		orders = append(orders, o)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Exec failed: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(orders)
 }
